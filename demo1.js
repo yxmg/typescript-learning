1a1149a3f4f12c25b59d34b2772063e362c9f917
guanmz <15900121620@163.com>
2021-03-15 08:09:49 +0800

    :sparkles: feat: demo1-类型定义演示

---
+++ demo1.js
@@ -0,0 +1,7 @@
+/*
+ * @Description: 类型定义
+ * @Author: 管铭钊
+ * @Date: 2021/3/15
+ */
+let a = 123
+a = '123'   // correct
---
+++ demo1.ts
@@ -0,0 +1,6 @@
+/**
+ *Created by 夜雪暮歌 on 2021/3/15
+ **/
+let a: number = 123
+// a = '123'   // Type 'string' is not assignable to type 'number'
+a = 234
