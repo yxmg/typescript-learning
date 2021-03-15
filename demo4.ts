0b8b6d8dfaae81ace0a61931d43ec6fdf5287611
guanmz <15900121620@163.com>
2021-03-15 22:15:44 +0800

    :sparkles: feat(demo4): 新增函数相关类型注解演示

---
+++ demo4.ts
@@ -0,0 +1,31 @@
+/**
+ *Created by 夜雪暮歌 on 2021/3/15
+ **/
+// 函数相关类型注解
+// 1、函数返回值必须注解，才能保证最终返回值符合预期
+function getTotal(num1: number, num2: number): number {
+    // return num1 + num2 + '' // 推断出是string，但不符合预期
+    return num1 + num2
+}
+
+// 2、void表示空，没有返回值
+function print(message: string): void {
+    console.log(message, "message")
+}
+
+// 3、never表示从不返回，常见于跳出函数
+function errorEmitter(): never {
+    throw new Error('不会往下执行')
+    console.log(1, "1")
+}
+
+// 4、函数参数解构时请在对象上注解
+// function add({ num1: number, num2:number }): number { // 没有这种写法！
+function add({ num1, num2 }: { num1: number, num2: number }): number {
+    return num1 + num2
+}
+
+const total = getTotal(1, 2)
+print('Hello World!')
+const addResult = add({ num1: 1, num2: 2 })
+
