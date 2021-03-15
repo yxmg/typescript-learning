447b9cde9ab5e27d19472a9ff65525ca10e1a979
guanmz <15900121620@163.com>
2021-03-15 08:15:21 +0800

    :sparkles: feat(demo1): 新增自定义静态类型演示

--- demo1.ts
+++ demo1.ts
@@ -1,6 +1,14 @@
 /**
  *Created by 夜雪暮歌 on 2021/3/15
  **/
+// 一般静态类型
 let a: number = 123
 // a = '123'   // Type 'string' is not assignable to type 'number'
 a = 234
+
+// 自定义静态类型
+interface Person {
+    name: string
+}
+
+const programmer: Person = { name: 'GMZ' } // 必须有name
