1fa175e3accceabc33d5ab36d16f454bbe48294a
guanmz <15900121620@163.com>
2021-03-15 21:36:48 +0800

    :sparkles: feat: 新增基本类型和对象类型演示

---
+++ demo2.ts
@@ -0,0 +1,37 @@
+/**
+ *Created by 夜雪暮歌 on 2021/3/15
+ **/
+
+// 基本类型
+// JS：number、string、boolean、undefined、symbol、bigint
+// TS：void、never
+const age: number = 24
+const username: string = 'yxmg'
+const isProgrammer: boolean = true
+
+// 对象类型
+// 对象
+const info: {
+    age: number,
+    username: string,
+    isProgrammer: boolean
+} = {
+    age: 24,
+    username: '123',
+    isProgrammer: true
+}
+
+// 类
+class Programmer {
+}
+
+const yxmg: Programmer = new Programmer()
+
+// 类型数组
+const hobby: stirng[] = ['game', 'code']
+
+// 函数
+const coding: () => string = () => {
+    return 'Hello world'
+}
+
