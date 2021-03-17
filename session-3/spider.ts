34091d22900a6fa1117b54c9a93978f66f20b1ba
guanmz <15900121620@163.com>
2021-03-17 23:12:11 +0800

    :sparkles: feat(spider): 新增爬取 慕课网-新上好课 课程

--- session-3/spider.ts
+++ session-3/spider.ts
@@ -4,18 +4,50 @@
  * @Date: 2021/3/17
  */
 import superAgent from 'superagent'
+import cheerio from 'cheerio'
+
+interface Course {
+    title: string,
+    difficulty: string,
+    signUpCount: string,
+    price: string,
+    originPrice: string
+}
+
+interface CourseResult {
+    type: string,
+    list: Course[]
+}
 
 class Spider {
-    private rawHtml = ''
+    private async getCourseInfo() {
+        const { text: html } = await this.getRawHtml()
+        const $ = cheerio.load(html);
+        const typeElList = $('.new-course .list')
+        const result: CourseResult[] = []
+        typeElList.each((index, type) => {
+            const courseElList = $(type).find('.item')
+            const courseList: Course[] = []
+            courseElList.each((index, course) => {
+                const title = $(course).find('.title').eq(0).text()
+                const [difficulty, signUpText] =
+                    $(course).find('.difficulty').eq(0).text().split(' · ')
+                const price = $(course).find('.price').eq(0).text()
+                const originPrice = $(course).find('.origin-price').eq(0).text()
+                const signUpCount = signUpText.replace(/[^\d]+/g, '')
+                courseList.push({ title, difficulty, price, originPrice, signUpCount })
+            })
+            result.push({ type: $(type).data('type'), list: courseList })
+        })
+        console.log(JSON.stringify(result), "JSON.stringify(result)")
+    }
 
-    private async getRawHtml() {
-        const result = await superAgent.get(url)
-        this.rawHtml = result.text
-        console.log(this.rawHtml, "this.rawHtml")
+    private getRawHtml() {
+        return superAgent.get(url)
     }
 
     constructor(url: string) {
-        this.getRawHtml()
+        this.getCourseInfo()
     }
 }
 
