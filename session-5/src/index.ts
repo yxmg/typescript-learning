a28c5a8e2afec3cbed3ca71fe8b713d7035d1e65
guanmz <15900121620@163.com>
2021-03-22 22:39:58 +0800

    :sparkles: feat: 新增简易密码验证演示

--- session-5/src/index.ts
+++ session-5/src/index.ts
@@ -3,8 +3,10 @@
  **/
 import express from 'express'
 import router from "./router";
+import bodyParser from 'body-parser'
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(router)
 app.listen(7001, () => {
     console.log('server is running')
--- session-5/src/router.ts
+++ session-5/src/router.ts
@@ -6,11 +6,16 @@ import {Router, Request, Response} from 'express'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send(`<html><body><form action="/spider" method="post"><input type="password" name="password"><button>提交</button></form></body></html>`)
 })
 
-router.get('/spider', (req: Request, res: Response) => {
-    res.send('Goodbye World!')
+router.post('/spider', (req: Request, res: Response) => {
+    const password = req.body.password
+    if (password === '123') {
+        res.send('Correct password!')
+    } else {
+        res.send('Wrong password!')
+    }
 })
 
 export default router
