c7cbd1830f9be35f82c63f409ff7831986afd5a7
guanmz <15900121620@163.com>
2021-03-28 22:49:44 +0800

    :sparkles: feat: 新增react-router-dom路由

--- session-8/src/App.tsx
+++ session-8/src/App.tsx
@@ -1,53 +1,16 @@
 import React from 'react';
-import './login.css'
-
-import {Form, Input, Button} from 'antd';
-import {LockOutlined} from '@ant-design/icons';
-
-const NormalLoginForm = () => {
-    const onFinish = (values: string) => {
-        console.log('Received values of form: ', values);
-    }
-
-    return (
-        <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Password!',
-                    },
-                ]}
-            >
-                <Input
-                    prefix={<LockOutlined className="site-form-item-icon"/>}
-                    type="password"
-                    placeholder="Password"
-                />
-            </Form.Item>
-
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                </Button>
-            </Form.Item>
-        </Form>
-    )
-}
+import Login from './views/login'
+import {HashRouter, Switch, Route, Redirect} from 'react-router-dom'
 
 function App() {
     return (
-        <div className="login-page">
-            <NormalLoginForm/>
-        </div>
+        <HashRouter>
+            <Switch>
+                <Route path='/login' exact component={Login}/>
+                {/* redirect 需要放下面=-= */}
+                <Redirect from="/" to="/login"/>
+            </Switch>
+        </HashRouter>
     );
 }
 
